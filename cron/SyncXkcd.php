b6f5c1788c97478b507333232e152f3aad6bffac
Chris White <chris.white@xdesign.com>
2016-04-16 00:48:00 +0100

    cron script to sync with xkcd

---
+++ cron/SyncXkcd.php
@@ -0,0 +1,38 @@
+<?php
+
+class SyncXkcd
+{
+    const URL = 'http://xkcd.com/%s/info.0.json';
+    const COMICS_PATH = '%s/../storage/%s.json';
+
+    public function run()
+    {
+        echo "Starting XKCD sync\n";
+        $id = 1;
+
+        while (true) {
+            if (file_exists(sprintf(self::COMICS_PATH, __DIR__, $id))) {
+                echo "Comic $id exists, skipping\n";
+                $id++;
+                continue;
+            }
+
+            echo "Requesting comic $id\n";
+
+            $json = @file_get_contents(sprintf(self::URL, $id));
+
+            if (!$json) {
+                echo "Comic $id doesn't seem to exist, stopping\n";
+                break;
+            }
+
+            file_put_contents(sprintf(self::COMICS_PATH, __DIR__, $id), $json);
+            $id++;
+        }
+
+        echo "Finished\n";
+    }
+}
+
+$class = new SyncXkcd();
+$class->run();
