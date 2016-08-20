54f757e833071e2c25f4d378c26215c21dab5c12
Chris White <chris@cwhite.me>
2016-08-21 00:51:07 +0100

    move typescript options to tsconfig.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,17 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var mocha = require('gulp-mocha');
 
-// TODO: make this use tsconfig.json
-// var tsProject = ts.createProject('tsconfig.json');
+var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('build', function() {
     return gulp.src('src/**/*.ts', {base: '.'})
-        .pipe(ts({
-            "target": "es6",
-            "module": "commonjs",
-            "moduleResolution": "node",
-            "experimentalDecorators": true
-        }))
+        .pipe(ts(tsProject))
         .pipe(gulp.dest('.'));
 });
 
 gulp.task('test', ['build'], function() {
     return gulp.src('./test/**/*.ts', {base: '.'})
-        .pipe(ts({
-            "target": "es6",
-            "module": "commonjs",
-            "moduleResolution": "node",
-            "experimentalDecorators": true
-        }))
+        .pipe(ts(tsProject))
         .pipe(gulp.dest('.'))
         .pipe(mocha({
             reporter: 'nyan',
