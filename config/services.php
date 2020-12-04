8dee04d6272db963c9d2a1f475220a281d353b61
Sven Luijten <svenluijten1995@gmail.com>
2020-12-04 17:25:08 +0100

    Clean up .env.example and config

--- config/services.php
+++ config/services.php
@@ -14,27 +14,6 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-    ],
-
-    'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => 'us-east-1',
-    ],
-
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
-    ],
-
-    'stripe' => [
-        'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-    ],
-
     'google' => [
         'app_id' => env('GOOGLE_APP_ID'),
         'key' => env('GOOGLE_API_KEY')
